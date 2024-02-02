0bd7c766cbacefbb1ec2f38dedef475db2668285
franjaluga <playaimpuestos@gmail.com>
2024-02-02 13:27:24 -0300

    insertion-sort algorithm in java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //You can add more numbers----------->
+        int limit = 5;
+
+        int[] A = new int[limit];
+
+        // Change numbers from here ---------->
+        A[0] = 75;
+        A[1] = 35;
+        A[2] = 20;
+        A[3] = 2;
+        A[4] = 1;
+        //<-------------------------------------
+
+        for (int j = 1; j < A.length ; j++){
+            int key = A[j];
+            int i = j - 1;
+
+            while( i >= 0 && A[i]>key){
+                A[i+1] = A[i];
+                i = i - 1;
+            }
+            A[i+1] = key;
+        }
+
+        // Check ordered Array------------------>
+        for(int m = 0 ; m < A.length; m++){
+            System.out.println(A[m]);
+        }
+
     }
 }
